03f3b4ff4b4d62fad9a2a3e8b39cf7f9f2d0e120
Marcos Mota <60157172+marcmotta@users.noreply.github.com>
2025-07-27 11:10:50 +0700

    Add smartvelocity.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ internal/smartvelocity/smartvelocity.go
@@ -0,0 +1,36 @@
+// internal/smartvelocity/smartvelocity.go
+package smartvelocity
+
+import (
+	"log"
+	"os"
+)
+
+type App struct {
+	verbose bool
+	logger  *log.Logger
+}
+
+func NewApp(verbose bool) *App {
+	app := &App{
+		verbose: verbose,
+		logger:  log.New(os.Stdout, "", log.LstdFlags),
+	}
+
+	if verbose {
+		app.logger.SetPrefix("[DEBUG] ")
+	} else {
+		app.logger.SetPrefix("[INFO] ")
+	}
+
+	return app
+}
+
+func (a *App) Run() error {
+	a.logger.Printf("Starting SmartVelocity processing")
+
+	// Add your implementation here
+
+	a.logger.Println("Processing completed successfully")
+	return nil
+}
